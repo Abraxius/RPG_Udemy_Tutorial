4a8c3e6c024149dd27f74453c8f365a8a6298dc2
Abraxas <josti4a@t-online.de>
2019-11-08 14:29:13 +0100

    Abschnitt 1/2
    - Abschnitt 1 fertig gemacht
    - Abschnitt 2 fertig gemacht
    - Character kann nun laufen
    - Animation eingepflegt
    - Ordnerstruktur angepasst
    - Abschnit 3 übersprungen (weil GDD)

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()   //Sorgt für die Animation
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity); //konventiert es von global in local
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed); //Setzt den Animator Wert auf den jeweiligen Speed wert
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}

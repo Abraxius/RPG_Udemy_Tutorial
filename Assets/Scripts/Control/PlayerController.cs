8f7686c30a46b1b01170a9e511b684041489b97e
Alexander <josti4a@t-online.de>
2019-11-12 14:16:09 +0100

    Combat
    - msg kommt wenn wenn man einen Enemy attackieren möchte
    - Vererbungen eingepflegt (namespace)
    - HINWEIS: Die FIghter.cs Verlinkung funktioniert noch nicht!!!

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,12 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Control 
 {
 public class PlayerController : MonoBehaviour
 {
     private void Update()
+    {
+        InteractWithCombat();
+        InteractWithMovement();
+    }
+
+    private void InteractWithCombat()
+    {
+        RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+        foreach (RaycastHit hit in hits) //guckt ob ein angeklicktes Objekt das CombatTarget.cs besitzt
+        {
+            CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+            if (target == null) continue; //wenn target == null, schleife verlassen
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                GetComponent<Fighter>().Attack(target);
+            }
+        }
+    }
+
+    private void InteractWithMovement() 
     {
         if (Input.GetMouseButton(0))
         {
@@ -17,12 +39,17 @@ public class PlayerController : MonoBehaviour
 
     private void MoveToCursor()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+        if (hasHit)
         {
             GetComponent<Mover>().MoveTo(hit.point);
         }
+    }
+
+    private static Ray GetMouseRay() 
+    {
+        return Camera.main.ScreenPointToRay(Input.mousePosition);
     }
 }
 }
